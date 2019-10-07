069c3599ea8db68f1018ce18a6c70cf9993450bc
luantm <luantm@debian>
2019-10-07 22:37:16 +0700

    Bao cao tuan 2

--- Recipes/app/src/main/java/com/example/recipes/app/category/CategoryFragment.java
+++ Recipes/app/src/main/java/com/example/recipes/app/category/CategoryFragment.java
@@ -1,6 +1,7 @@
 package com.example.recipes.app.category;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,14 +10,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.recipes.R;
+import com.example.recipes.app.recipe.RecipeActivity;
 import com.example.recipes.model.Area;
 import com.example.recipes.model.Category;
+import com.example.recipes.model.Recipe;
 
 import java.util.List;
 
-public class CategoryFragment extends Fragment implements ICategoryFragment {
+public class CategoryFragment extends Fragment implements ICategoryFragment, View.OnClickListener {
+    private ImageView mFull;
 
     public static CategoryFragment newInstance() {
         return new CategoryFragment();
@@ -31,7 +36,14 @@ public class CategoryFragment extends Fragment implements ICategoryFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.category_frag, container, false);
+        View inflate = inflater.inflate(R.layout.category_frag, container, false);
+        mapping(inflate);
+        return inflate;
+    }
+
+    private void mapping(View view) {
+        mFull = view.findViewById(R.id.full);
+        mFull.setOnClickListener(this);
     }
 
     @Override
@@ -42,5 +54,13 @@ public class CategoryFragment extends Fragment implements ICategoryFragment {
     @Override
     public void onDisplayList(List<Category> categories) {
         //TODO on display list
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mFull) {
+            Intent intent = new Intent(getActivity(), RecipeActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- Recipes/app/src/main/java/com/example/recipes/app/recipe/RecipeFragment.java
+++ Recipes/app/src/main/java/com/example/recipes/app/recipe/RecipeFragment.java
@@ -1,6 +1,7 @@
 package com.example.recipes.app.recipe;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,11 +9,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.example.recipes.BaseFragment;
 import com.example.recipes.R;
+import com.example.recipes.app.recipe_detail.RecipeDetailActivity;
+import com.example.recipes.model.Recipe;
 
-public class RecipeFragment extends Fragment {
+import java.util.List;
 
+public class RecipeFragment extends BaseFragment implements IRecipeFragment, View.OnClickListener {
+    private ImageView mFull;
 
     public RecipeFragment() {
         // Required empty public constructor
@@ -26,7 +33,31 @@ public class RecipeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.recipe_frag, container, false);
+        View inflate = inflater.inflate(R.layout.recipe_frag, container, false);
+        mapping(inflate);
+        return inflate;
     }
 
+    private void mapping(View view) {
+        mFull = view.findViewById(R.id.full);
+        mFull.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mFull) {
+            Intent intent = new Intent(getActivityNonNull(), RecipeDetailActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onGetList() {
+
+    }
+
+    @Override
+    public void onDisplayList(List<Recipe> recipes) {
+
+    }
 }
