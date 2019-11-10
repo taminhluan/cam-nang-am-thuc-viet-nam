ec65c3658321fe3ac132321ab7bce4eef565d4cc
luantm <ngoalongtb001@gmail.com>
2019-11-11 00:03:14 +0700

    update

--- Recipes/app/src/main/java/com/example/recipes/app/recipe/RecipeFragment.java
+++ Recipes/app/src/main/java/com/example/recipes/app/recipe/RecipeFragment.java
@@ -1,36 +1,53 @@
 package com.example.recipes.app.recipe;
 
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.recipes.BaseFragment;
 import com.example.recipes.R;
+import com.example.recipes.admin.recipe.IRecipeAdminDetailFragment;
 import com.example.recipes.admin.recipe.IRecipeAdminFragment;
+import com.example.recipes.admin.recipe.RecipeAdminDetailFragment;
 import com.example.recipes.admin.recipe.RecipeAdminFragment;
 import com.example.recipes.app.recipe_detail.RecipeDetailActivity;
 import com.example.recipes.constant.AppConstant;
 import com.example.recipes.db.AppDatabase;
+import com.example.recipes.model.Category;
+import com.example.recipes.model.Event;
+import com.example.recipes.model.Area;
 import com.example.recipes.model.Recipe;
 import com.squareup.picasso.Picasso;
 
+import java.io.Serializable;
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RecipeFragment extends BaseFragment implements IRecipeFragment, View.OnClickListener {
     private RecyclerView mRv;
+
+    private Area mArea;
+    private Event mEvent;
+    private Category mCategory;
 
     public RecipeFragment() {
         // Required empty public constructor
@@ -46,8 +63,20 @@ public class RecipeFragment extends BaseFragment implements IRecipeFragment, Vie
         // Inflate the layout for this fragment
         View inflate = inflater.inflate(R.layout.recipe_frag, container, false);
         mapping(inflate);
+        
+        getDataFromIntent();
+        
         onGetList();
         return inflate;
+    }
+
+    private void getDataFromIntent() {
+        Intent intent = getActivityNonNull().getIntent();
+
+        mEvent = (Event) intent.getSerializableExtra(AppConstant.EVENT);
+        mArea = (Area) intent.getSerializableExtra(AppConstant.AREA);
+        mCategory = (Category) intent.getSerializableExtra(AppConstant.CATEGORY);
+
     }
 
     private void mapping(View view) {
@@ -79,6 +108,203 @@ public class RecipeFragment extends BaseFragment implements IRecipeFragment, Vie
 
     @Override
     public void openPopupFilter() {
+        FilterDialogFragment filterDialogFragment =  new FilterDialogFragment(getActivityNonNull(), mArea, mCategory, mEvent);
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        filterDialogFragment.show(ft, "dialog");
+
+    }
+
+    public static class FilterDialogFragment extends DialogFragment {
+        private List<Area> mAreas;
+        private List<Category> mCategories;
+        private List<Event> mEvents;
+
+        private ArrayAdapter<String> mSpinnerAreaAdapter;
+        private ArrayAdapter<String> mSpinnerCategoryAdapter;
+        private ArrayAdapter<String> mSpinnerEventAdapter;
+
+        private Spinner mSpArea;
+        private Spinner mSpCategory;
+        private Spinner mSpEvent;
+
+        private Area mArea;
+        private Category mCategory;
+        private Event mEvent;
+
+        private Activity mActivity;
+
+        public FilterDialogFragment(Activity activity, Area area, Category category, Event event) {
+            mActivity = activity;
+            mArea = area;
+            mCategory = category;
+            mEvent = event;
+        }
+
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View root = inflater.inflate(R.layout.recipe_dialog_frag, container, false);
+
+            mapping(root);
+
+            getListAreas();
+
+            getListCategories();
+
+            getListEvents();
+
+            return root;
+        }
+
+        private void mapping(View view) {
+            mSpArea = view.findViewById(R.id.spArea);
+            mSpCategory = view.findViewById(R.id.spCategory);
+            mSpEvent = view.findViewById(R.id.spEvent);
+        }
+
+        public void getListAreas() {
+            new FilterDialogFragment.GetListAreasAsyncTask(this).execute();
+        }
+
+        public void getListCategories() {
+            new FilterDialogFragment.GetListCategoriesAsyncTask(this).execute();
+        }
+
+        public void getListEvents() {
+            new FilterDialogFragment.GetListEventsAsyncTask(this).execute();
+        }
+
+        public void displayListAreas(List<Area> areas) {
+            mAreas = areas;
+            List<String> areaAsStrings = new ArrayList<>();
+            for (Area area: areas ) {
+                areaAsStrings.add(area.getName());
+            }
+            mSpinnerAreaAdapter = new ArrayAdapter<String>(mActivity,
+                    android.R.layout.simple_spinner_item, areaAsStrings);
+            mSpinnerAreaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            mSpArea.setAdapter(mSpinnerAreaAdapter);
+
+            if (mArea != null) {
+                for (int i = 0; i < mAreas.size(); i++) {
+                    Area area = mAreas.get(i);
+                    if (area.getUid() == mArea.getUid()) {
+                        mSpArea.setSelection(i);
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void displayListCategories(List<Category> categories) {
+            mCategories = categories;
+            List<String> categoryAsStrings = new ArrayList<>();
+            for (Category category: categories) {
+                categoryAsStrings.add(category.getName());
+            }
+            mSpinnerCategoryAdapter = new ArrayAdapter<String>(mActivity,
+                    android.R.layout.simple_spinner_item, categoryAsStrings);
+            mSpinnerCategoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            mSpCategory.setAdapter(mSpinnerCategoryAdapter);
+
+            if (mCategory != null) {
+                for (int i = 0; i < mCategories.size(); i++) {
+                    Category category = mCategories.get(i);
+                    if (category.getUid() == mCategory.getUid()) {
+                        mSpCategory.setSelection(i);
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void displayListEvents(List<Event> events) {
+            mEvents = events;
+            List<String> eventAsStrings = new ArrayList<>();
+            for (Event event: events) {
+                eventAsStrings.add(event.getName());
+            }
+            mSpinnerEventAdapter = new ArrayAdapter<String>(mActivity,
+                    android.R.layout.simple_spinner_item, eventAsStrings);
+            mSpinnerEventAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            mSpEvent.setAdapter(mSpinnerEventAdapter);
+
+            if (mEvent != null) {
+                for (int i = 0; i < mEvents.size(); i++) {
+                    Event event = mEvents.get(i);
+                    if (event.getUid() == mEvent.getUid()) {
+                        mSpEvent.setSelection(i);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private class GetListAreasAsyncTask extends AsyncTask<Void, Void, List<Area>> {
+            private WeakReference<FilterDialogFragment> mWeakReferenceFragment;
+            public GetListAreasAsyncTask(FilterDialogFragment fragment) {
+                mWeakReferenceFragment = new WeakReference<>(fragment);
+            }
+
+            @Override
+            protected List<Area> doInBackground(Void... voids) {
+                AppDatabase db = AppDatabase.getInstance(mActivity);
+                List<Area> all = db.getAreaDao().getAll();
+                return all;
+            }
+
+            @Override
+            protected void onPostExecute(List<Area> areas) {
+                super.onPostExecute(areas);
+                mWeakReferenceFragment.get().displayListAreas(areas);
+            }
+        }
+        private class GetListCategoriesAsyncTask extends AsyncTask<Void, Void, List<Category>> {
+            private WeakReference<FilterDialogFragment> mWeakReferenceFragment;
+            public GetListCategoriesAsyncTask(FilterDialogFragment fragment) {
+                mWeakReferenceFragment = new WeakReference<>(fragment);
+            }
+
+            @Override
+            protected List<Category> doInBackground(Void... voids) {
+                AppDatabase db = AppDatabase.getInstance(mActivity);
+                List<Category> all = db.getCategoryDao().getAll();
+                return all;
+            }
+
+            @Override
+            protected void onPostExecute(List<Category> categories) {
+                super.onPostExecute(categories);
+                mWeakReferenceFragment.get().displayListCategories(categories);
+            }
+        }
+
+        private class GetListEventsAsyncTask extends AsyncTask<Void, Void, List<Event>> {
+            private WeakReference<FilterDialogFragment> mWeakReferenceFragment;
+            public GetListEventsAsyncTask(FilterDialogFragment fragment) {
+                mWeakReferenceFragment = new WeakReference<>(fragment);
+            }
+
+            @Override
+            protected List<Event> doInBackground(Void... voids) {
+                AppDatabase db = AppDatabase.getInstance(mActivity);
+                List<Event> all = db.getEventDao().getAll();
+                return all;
+            }
+
+            @Override
+            protected void onPostExecute(List<Event> events) {
+                super.onPostExecute(events);
+                mWeakReferenceFragment.get().displayListEvents(events);
+            }
+        }
+
 
     }
 
